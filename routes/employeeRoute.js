46ec810258f126b4b4eeaa39168f9ed5e3015a3b
Malachai Vargas <mvarg712stu.southtexascollege.edu>
2024-11-11 22:25:24 -0600

    delete is work

--- routes/employeeRoute.js
+++ routes/employeeRoute.js
@@ -1,8 +1,10 @@
 import express from "express"
-import { create, fetch} from "../controller/employeeController.js"
+import { create, fetch, update} from "../controller/employeeController.js"
 const route = express.Router()
 
 route.post("/create", create)
 route.get("/fetch", fetch)
+route.put("/update/:id", update)
+//route.delete("/delete/:id", deleteEmployee)
 
 export default route
